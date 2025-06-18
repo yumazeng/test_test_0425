081eb2ba40df0a8295aa5147f1e2e1164650cc54
yumazeng <chloetw@gmail.com>
2025-06-18 13:26:53 +0800

    modify

---
+++ demo2/check_tf.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+print("TensorFlow version:", tf.__version__)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+print("Successfully imported Keras components.")
